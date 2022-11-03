1bf0eec65cc3dca970b7243309ec047211e553e2
Mo Helmy <helmysmile3@gmail.com>
2022-11-03 04:17:31 +0000

    edit node engine

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,24 @@ const address = `http://localhost:${port}`;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'X-Access-Token',
+      'Authorization',
+      'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Methods'
+    ],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    preflightContinue: true,
+    origin: '*'
+  })
+);
 
 app.use('/api', routes);
 
